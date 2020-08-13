7d2c3ebf8f9dc7be2b103e6c5e08ce341d9e58f2
Infotronica <65802795+Infotronica@users.noreply.github.com>
2020-08-13 04:06:54 -0500

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -23,26 +23,16 @@ MainWindow::MainWindow(QWidget *parent) :
         urlRemoveSVG - for remove svg files stored in the server
     */
 
-    db_name="infotron_countries"; // the database name
-    usr_db="infotron_admin"; // the user for this database
-    pwd_db="M7SWcVsUJa]#"; // the password of user
-
-    urlWS="http://infotronicamx.com/for_Navjeet/from_network_json_2/webservice/webservice.php";
-    urlUploadSVG="http://infotronicamx.com/for_Navjeet/from_network_json_2/webservice/upload_svg.php";
-    urlDownloadSVG="http://infotronicamx.com/for_Navjeet/from_network_json_2/webservice/download_svg.php";
-    urlRemoveSVG="http://infotronicamx.com/for_Navjeet/from_network_json_2/webservice/remove_svg.php";
-    remoteFolder="country_flags"; // remote folder for store the svg files
-/*
-    db_name="miserver_countries"; // the database name
-    usr_db="miserver_admin"; // the user for this database
-    pwd_db="#W0UliZsJZsu"; // the password of user
+    db_name="countries"; // the database name
+    usr_db="your user"; // the user for this database
+    pwd_db="your password"; // the password of user
 
     urlWS="http://mi-server-de-pruebas.com/for_Navjeet/from_network_json_2/webservice/webservice.php";
     urlUploadSVG="http://mi-server-de-pruebas.com/for_Navjeet/from_network_json_2/webservice/upload_svg.php";
     urlDownloadSVG="http://mi-server-de-pruebas.com/for_Navjeet/from_network_json_2/webservice/download_svg.php";
     urlRemoveSVG="http://mi-server-de-pruebas.com/for_Navjeet/from_network_json_2/webservice/remove_svg.php";
     remoteFolder="country_flags"; // remote folder for store the svg files
-*/
+
     listRecords();
 }
 
